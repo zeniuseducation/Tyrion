1fcff7a6e0501b2b52886dba87942a66d15855b7
squest <sabdaps@gmail.com>
2015-12-07 23:29:54 +0700

    big bang

---
+++ src/tyrion/core.clj
@@ -0,0 +1,6 @@
+(ns tyrion.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/tyrion/core_test.clj
@@ -0,0 +1,7 @@
+(ns tyrion.core-test
+  (:require [clojure.test :refer :all]
+            [tyrion.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
