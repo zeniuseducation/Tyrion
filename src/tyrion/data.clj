ec5441264e15f7e50ba275ce398a1a5f2726bc4f
squest <sabdaps@gmail.com>
2015-12-19 07:51:42 +0700

    added some data

---
+++ src/tyrion/data.clj
@@ -0,0 +1,21 @@
+(ns tyrion.data
+  (:require [clojure.string :as cs]))
+
+(def dataset
+  {:iris {:name        "Iris"
+          :description "Iris dataset, good for clustering/classifications, or basic stats"
+          :file        "./data/iris.edn"}})
+
+(defn load-meta
+  [dataset-key]
+  (get dataset dataset-key :iris))
+
+(defn load-data
+  ([dataset-key]
+   (let [{:keys [file] :as d} (load-meta dataset-key)]
+     (assoc d :data (->> (slurp file) read-string))))
+  ([dataset-key data-only?]
+   (let [{:keys [file]} (load-meta dataset-key)]
+     (->> (slurp file) read-string))))
+
+
